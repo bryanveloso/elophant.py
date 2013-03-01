f1c489720a6d11ae508d3dcac13ef73b2e0d7b3e
Bryan Veloso <bryan@revyver.com>
2013-02-28 20:41:45 -0800

    An setup.py file. This is 0.0.1.

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
+    sys.exit()
+
+
+setup(
+    name='elophant',
+    version='0.0.1',
+    description='API Wrapper for Elophant(.com)',
+    long_description=open('README.md').read(),
+    author='Bryan Veloso',
+    author_email='bryan@revyver.com',
+    url='https://github.com/bryanveloso/elophant.py',
+    py_modules=['elophant'],
+    install_requires=['requests'],
+    license='MIT',
+)
